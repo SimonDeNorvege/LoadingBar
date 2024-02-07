5cb42d45faef63502ae4ac1f6c888a9f20d3aa1c
PierreRoumane <60694178+PierreRoumane@users.noreply.github.com>
2024-02-07 13:58:35 +0100

    <rev> completed first version

--- LoadingBar.java
+++ LoadingBar.java
@@ -10,45 +10,66 @@ public class LoadingBar extends JPanel {
 
         JProgressBar pbar;
 
-        static final int MY_MINIMUM = 0;
-        static final int MY_MAXIMUM = 100;
+        public long courseLength = 4680; //4680000
+        public long startTime;
+        public long endTime;
+        public static final int MY_MINIMUM = 0;
+        public static final int MY_MAXIMUM = 100;
 
         public LoadingBar() {
+            this.startTime = System.currentTimeMillis();
             // initialize Progress Bar
-            pbar = new JProgressBar();
-            pbar.setMinimum(MY_MINIMUM);
-            pbar.setMaximum(MY_MAXIMUM);
+            this.endTime = startTime + courseLength;
+            this.pbar = new JProgressBar();
+            this.pbar.setMinimum(MY_MINIMUM);
+            this.pbar.setMaximum(MY_MAXIMUM);
             // add to JPanel
             add(pbar);
+        }
+
+        public long timeElapsed(long currenTime, long starTime)
+        {
+            return(currenTime - starTime);
         }
 
         public void updateBar(int newValue) {
             pbar.setValue(newValue);
         }
+            public int getProgress()
+            {
+                double currentTime = timeElapsed(System.currentTimeMillis(), this.startTime);
+                double progressTime = currentTime / (this.courseLength);
+                return (int) (progressTime * 100);
+            }
 
-        public static void main(String args[]) {
+    public static void main(String args[]) {
 
-            final LoadingBar it = new LoadingBar();
+        final LoadingBar loadingBar = new LoadingBar();
 
-            JFrame frame = new JFrame("Loading Bar Example");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setContentPane(it);
-            frame.pack();
-            frame.setVisible(true);
+        JFrame frame = new JFrame("Loading Bar Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(loadingBar);
+        frame.pack();
+        frame.setVisible(true);
 
-            // run a loop to demonstrate raising
-            for (int i = MY_MINIMUM; i <= MY_MAXIMUM; i++) {
-                final int percent = i;
-                try {
+        // run a loop to demonstrate raising
+        while (loadingBar.getProgress() < 100) {
+            loadingBar.updateBar(loadingBar.getProgress());
+            
+        }
+    }
+}
+
+
+    /*
+    try {
                     SwingUtilities.invokeLater(new Runnable() {
                         public void run() {
-                            it.updateBar(percent);
+                            loadingBar.updateBar(percent);
                         }
-                    });
+                   });
                     java.lang.Thread.sleep(100);
                 } catch (InterruptedException e) {
                     ;
                 }
-            }
-        }
-    }
+     */
